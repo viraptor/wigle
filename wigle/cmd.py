52375ec2fb52c680fa5d4421fb1c33d915311811
Stanisław Pitucha <viraptor@gmail.com>
2015-03-11 19:59:44 +1100

    Add specific cmdline error for exceeded quota

--- wigle/cmd.py
+++ wigle/cmd.py
@@ -1,4 +1,4 @@
-from . import Wigle
+from . import Wigle, WigleRatelimitExceeded
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -33,11 +33,15 @@ def search():
     def notify_new_page(first):
         print("Downloading new page (records from %i)" % first)
 
-    results = Wigle(args.user, args.password).search(
-        ssid=args.ssid,
-        bssid=args.bssid,
-        on_new_page=notify_new_page,
-        max_results=args.max)
+    try:
+        results = Wigle(args.user, args.password).search(
+            ssid=args.ssid,
+            bssid=args.bssid,
+            on_new_page=notify_new_page,
+            max_results=args.max)
 
-    for result in results:
-        print("%(ssid)s, %(netid)s, %(trilat)s, %(trilong)s" % result)
+        for result in results:
+            print("%(ssid)s, %(netid)s, %(trilat)s, %(trilong)s" % result)
+
+    except WigleRatelimitExceeded:
+        print("Cannot query Wigle - exceeded number of allowed requests")
